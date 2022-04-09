a2730be4993025abe9945670094c399c73fe9802
geraldwogan <geraldwogan@gmail.com>
2022-04-09 14:54:04 +0100

    Open speed test SERPS

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link= 'https://www.google.com/search?q=internet+speed+test'
+
+# Selenium Web Driver specifically for chrome, download from here: https://chromedriver.chromium.org/downloads
+driver =  webdriver.Chrome('chromedriver.exe')
+
+# Open webpage
+driver.get(link)
+
+# Wait for page to load
+time.sleep(5)
