9392f6d1e97699e3a57ff21f1a780f42f02c8270
geraldwogan <geraldwogan@gmail.com>
2022-04-10 14:48:29 +0100

    Package speed test feature as a function

--- main.py
+++ main.py
@@ -4,29 +4,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Selenium Web Driver specifically for chrome, download from here: https://chromedriver.chromium.org/downloads
-driver = webdriver.Chrome('chromedriver.exe')
+def get_speed_test_results():
 
-# Open webpage
-driver.get('https://www.google.com/search?q=internet+speed+test')
-# Wait for page to load
-time.sleep(.5)
+    # Selenium Web Driver specifically for chrome, download from here: https://chromedriver.chromium.org/downloads
+    driver = webdriver.Chrome('chromedriver.exe')
 
-# Accept the cookie settings by clickin the 'I agree' button.
-e = driver.find_element(By.XPATH, '//button[contains(., "I agree")]')
-e.click()
-time.sleep(.5)
+    # Open webpage
+    driver.get('https://www.google.com/search?q=internet+speed+test')
+    # Wait for page to load
+    time.sleep(.5)
 
-# Click the 'Run Speed Test' button
-e = driver.find_element(By.XPATH, '//g-raised-button[contains(., "RUN SPEED TEST")]')
-e.click()
-time.sleep(35) # Speed test generally takes about 30 seconds, may swithch to using EC.presence_of_all_elements_located
-# e = WebDriverWait(driver,40).until(EC.presence_of_all_elements_located((By.XPATH, '//g-raised-button[contains(., "TEST AGAIN")]')))
+    # Accept the cookie settings by clickin the 'I agree' button.
+    e = driver.find_element(By.XPATH, '//button[contains(., "I agree")]')
+    e.click()
+    time.sleep(.5)
 
-# Get results of speed test
-speeds = driver.find_elements(By.CLASS_NAME,'spiqle')
-download = speeds[0].text
-upload = speeds[1].text
-print('d',download)
-print('u',upload)
+    # Click the 'Run Speed Test' button
+    e = driver.find_element(By.XPATH, '//g-raised-button[contains(., "RUN SPEED TEST")]')
+    e.click()
+    time.sleep(35) # Speed test generally takes about 30 seconds, may swithch to using EC.presence_of_all_elements_located
+    # e = WebDriverWait(driver,40).until(EC.presence_of_all_elements_located((By.XPATH, '//g-raised-button[contains(., "TEST AGAIN")]')))
 
+    # Get results of speed test
+    speeds = driver.find_elements(By.CLASS_NAME,'spiqle') # Returns a list like [download, upload]
+
+    print(f'Returned info:\nType@ {type(speeds)}\nLength: {len(speeds)} \nValues:')
+
+    for v in speeds:
+        print(v)
+        print(v.text)
+
+    download = speeds[0].text
+    upload = speeds[1].text
+    print('d',download)
+    print('u',upload)
+
+    return download, upload
+
+get_speed_test_results()
